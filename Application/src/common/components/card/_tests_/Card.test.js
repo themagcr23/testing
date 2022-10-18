28f09313dd35e257eece1caa67de55c3e2869258
miguel.ramos <miguel.ramos@sparkleit.pt>
2022-10-18 10:26:37 +0100

    teste

--- Application/src/common/components/card/_tests_/Card.test.js
+++ Application/src/common/components/card/_tests_/Card.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
-import Card from "../Card";
+import Card from '../Card';
 
 describe('random test', () => {
     it('test component', () => {
